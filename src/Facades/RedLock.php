33f2189ef03dd4ccb71eb407f4f83314bd3308b3
dankuck <dankuck@gmail.com>
2017-05-25 17:16:52 -0400

    Update RedLock.php

--- src/Facades/RedLock.php
+++ src/Facades/RedLock.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
- * This file is part of jwt-auth.
- *
- * (c) Sean Tymon <tymon148@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace LibiChai\RedLock\Facades;
+namespace ThatsUs\RedLock\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
