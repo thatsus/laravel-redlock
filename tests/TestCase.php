6a16273182ad7c9fc33da9e1404267e1c027de78
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2017-08-03 16:29:58 -0400

    Allow Laravel's default E_ALL to be the error reporting level. Should make us safe for all of our users' error levels too.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,17 +17,6 @@ class TestCase extends Base
 
         $app->make(Kernel::class)->bootstrap();
 
-        //$this->setTestEnvironment();
-
-        // don't want these kinds of error notifications
-        error_reporting(
-            E_ALL 
-            & ~ E_NOTICE 
-            //& ~ E_WARNING 
-            //& ~ E_DEPRECATED 
-            //& ~ E_STRICT
-        );
-
         return $app;
     }
 }
